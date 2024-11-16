7ea8f5cd59efbc357c02527b1a5999b55f6e1aa7
GinoSoccio007 <aiolosmedia26@gmail.com>
2024-11-15 21:36:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files
+// Serve static files from public directory
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -11,17 +11,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Create new project
-app.post('/api/projects', (req, res) => {
-    const { name, template } = req.body;
-    res.json({
-        id: Date.now(),
-        name,
-        template,
-        created: new Date()
-    });
-});
-
+// Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
